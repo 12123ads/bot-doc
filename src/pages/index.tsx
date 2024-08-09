d6a31d347308d7db2ed045302f938e8944a5761e
12123ads <3127086580@qq.com>
2024-08-09 19:04:49 +0800

    chore: 更新首页按钮链接到初始化页面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/init">
             点击查看Bot的指令~
           </Link>
         </div>
