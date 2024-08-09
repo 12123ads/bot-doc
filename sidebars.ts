584727e77635523dea9b7147b8c5a243e26defd0
12123ads <3127086580@qq.com>
2024-08-10 00:09:45 +0800

    chore: 更新文档初始化文件和配置文件

--- sidebars.ts
+++ sidebars.ts
@@ -2,7 +2,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
-    'init',
+    'start',
     {
       type: 'category',
       label: '游戏类指令',
